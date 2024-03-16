74f2232ab3f24820560e17432f0edaa4c105596a
Aijamal Jusupova <109210016+Aijus2022@users.noreply.github.com>
2024-03-16 16:30:17 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './Header';
+import Home from './Home';
+import Projects from './Projects';
+import Project from './Project';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route exact path="/home" element={<Home />} /> 
+          <Route exact path="/projects" element={<Projects />} /> 
+          <Route path="/projects/:projectId" element={<Project />} /> 
+          <Route exact path="/contact" element={<Contact />} /> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
